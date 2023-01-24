dd2445b0fbc32521ed64281c87c3c9cc9c95e4aa
Konstantin Ivanovic <121982642+Konstantin907@users.noreply.github.com>
2023-01-24 00:14:31 -0800

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,50 @@
+function q1() {
+    for(let i = 1; i <= 100; i++) {
+        
+        if( (i%3 === 0))
+        {
+            console.log("Fizz")
+        }
+        if( i % 5 === 0 ) {
+            console.log("Buzz")
+        }
+        if(  ( i % 3 === 0) && (i % 5 === 0) ) {
+            console.log("FizzBuzz")
+        }
+
+    }
+}
+
+function q2 () {
+    let sum = 0
+    for(let i = 1 ; i <= 100; i++) {
+
+        if( i % 2 == 0 ) {
+            sum = sum + i ;
+        }
+
+    }
+    console.log(sum)
+}
+
+var arr = [4, 18, 7, 13, 12]
+
+function q3 (arr) {
+
+    let largest = -Infinity
+ 
+    for (let i = 0; i < arr.length; i++) {
+        if( largest < arr[i] ) {
+            largest = arr[i]
+        }
+    }
+ 
+    console.log("Largest number is: "  + largest) 
+    
+}
+
+
+q1()
+q2()
+q3(arr)
+
